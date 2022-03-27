300f23ae41317d03e6015841149426b4f967b4de
Roman Pylypenko <rpylypenko@aligntech.com>
2022-03-27 09:44:03 +0300

    Created api client for tests

---
+++ tests/api_client.py
@@ -0,0 +1,46 @@
+import requests
+
+from requests import Response
+
+
+class ApiClient:
+
+    def __init__(self):
+        self.url = 'http://127.0.0.1:5000'
+
+    def get_home(self) -> Response:
+        return requests.get(
+            self.url
+        )
+
+    def post_create_store(self, name: str) -> Response:
+        return requests.post(
+            f'{self.url}/store',
+            json={
+                'name': name
+            }
+        )
+
+    def get_store(self, name: str) -> Response:
+        return requests.get(
+            f'{self.url}/store/{name}'
+        )
+
+    def get_stores(self) -> Response:
+        return requests.get(
+            f'{self.url}/store'
+        )
+
+    def post_create_item(self, store_name: str, name: str, price: float) -> Response:
+        return requests.post(
+            f'{self.url}/store/{store_name}/item',
+            json={
+                'name': name,
+                'price': price
+            }
+        )
+
+    def get_items(self, store_name: str) -> Response:
+        return requests.get(
+            f'{self.url}/store/{store_name}/item'
+        )
